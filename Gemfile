2bbe4ae5fed9d5233b240e6e8684f181830477df
Nishant <nishant301294@gmail.com>
2016-02-11 12:51:17 -0500

    Added template links and stabilized css

--- Gemfile
+++ Gemfile
@@ -2,5 +2,14 @@ source 'https://rubygems.org'
 
 gem 'roda'
 gem 'rake'
-gem 'rack-unreloader'
+gem 'dotenv'
+
+# Templates
 gem 'tilt'
+
+# CSS and JS
+gem 'sass'
+
+group :development do
+  gem 'rack-unreloader'
+end
--- index.rb
+++ index.rb
@@ -1,18 +1,22 @@
 require 'roda'
+require 'dotenv'
+Dotenv.load
 
 # Main landing page class
 class Index < Roda
 
   plugin :render
   plugin :static, ['/css']
-  plugin :assets, css:'main.css'
+  plugin :assets, css:'main.scss'
+  compile_assets unless ENV['RACK_ENV'] == 'development'
 
   route do |r|
     r.assets
 
     r.root do
+      puts ENV['RACK_ENV']
       @links = { Resume: '#', LinkedIn: 'https://www.linkedin.com/in/nishantshah94',
-        Github: 'https://github.com/nini1294' }
+        Github: 'https://github.com/nini1294'}
       @name = 'Nishant Shah'
       render('index')
     end
